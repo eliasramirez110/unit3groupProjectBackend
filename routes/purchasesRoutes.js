2e8348b5492e0a48d8844959e49b1de24d0801ad
Elias Ramirez <ramirezelias110@gmail.com>
2021-05-06 16:50:58 -0400

    purchase controller and router

---
+++ routes/purchasesRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const purchaseRouter = express.Router()
+const purchaseController = require('../controllers/purchasesController')
+
+purchaseRouter.post('/create', purchaseController.create)
+purchaseRouter.get('/getsneaker', purchaseController.store)
+module.exports = purchaseRouter
