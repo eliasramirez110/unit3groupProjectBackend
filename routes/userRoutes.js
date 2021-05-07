1af171013766381f9bc35c7c50b66ad90e6c2802
rz4life <rzalagbada@Abdulrazaqs-Air.cable.rcn.com>
2021-05-07 11:05:33 -0500

    small changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const userRouter = express.Router()
-const userController = require('../controllers/userController')
+const userController = require('../controllers/userControllers')
 
 userRouter.post('/signup', userController.create)
 userRouter.post('/login', userController.login)
 userRouter.delete('/destroy/:id', userController.destroy)
+userRouter.get('/verify/:userId', userController.verify)
 
 module.exports = userRouter
 
