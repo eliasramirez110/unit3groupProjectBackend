bfe19785714d0ea4383c9234eb74f57932a8c49e
rz4life <rzalagbada@Abdulrazaqs-Air.cable.rcn.com>
2021-05-08 19:20:10 -0500

    modified remove item from cart controller

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -3,6 +3,6 @@ const cartRouter = express.Router()
 const cartController = require('../controllers/cartControllers')
 
 cartRouter.get('/:id/products', cartController.getProducts)
-cartRouter.delete('/:id/products', cartController.removeProduct)
+cartRouter.delete('/:id/products/:productId', cartController.removeProduct)
 
 module.exports = cartRouter 
